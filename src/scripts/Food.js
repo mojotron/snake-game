6ecf1818a5efc968b4875692ece62127ec7f30c9
mojotron <igormalj@gmail.com>
2022-02-23 22:36:20 +0100

    Add inner html for apple element

--- src/scripts/Food.js
+++ src/scripts/Food.js
@@ -21,6 +21,12 @@ class Food {
   display(boardElement) {
     const foodElement = document.createElement('div');
     foodElement.classList.add('apple');
+    foodElement.innerHTML = `
+      <div class="apple__body body-1"></div>
+      <div class="apple__body body-2"></div>
+      <div class="peteljka"></div>
+      <div class="leef"></div>
+    `;
     foodElement.style.gridArea = `${this.#foodCoords.x + 1}/${
       this.#foodCoords.y + 1
     }/${this.#foodCoords.x + 2}/${this.#foodCoords.y + 2}`;
