9ef1e7cdd56008c95c57364fbeaafeb561741294
mojotron <igormalj@gmail.com>
2022-02-23 08:49:08 +0100

    Finish snake move style logic

--- src/scripts/Snake.js
+++ src/scripts/Snake.js
@@ -58,37 +58,52 @@ class Snake {
       // TODO snake style logic;
       if (i === 0) {
         snakeElement.classList.add('snake-head');
+        snakeElement.classList.add(`snake-rotate-${this.#snake[0].direction}`);
       } else if (i === this.#snake.length - 1) {
         snakeElement.classList.add('snake-tail');
+        const prev = this.#snake[i - 1].direction;
+        const curr = this.#snake[i].direction;
+        if (prev === curr)
+          snakeElement.classList.add(`snake-rotate-${ele.direction}`);
+        if (curr === 'left' && prev === 'down')
+          snakeElement.classList.add(`snake-rotate-down`);
+        if (curr === 'right' && prev === 'down')
+          snakeElement.classList.add(`snake-rotate-down`);
+        if (curr === 'left' && prev === 'up')
+          snakeElement.classList.add(`snake-rotate-up`);
+        if (curr === 'right' && prev === 'up')
+          snakeElement.classList.add('snake-rotate-up');
+        //
+        if (curr === 'up' && prev === 'right')
+          snakeElement.classList.add(`snake-rotate-right`);
+        if (curr === 'up' && prev === 'left')
+          snakeElement.classList.add(`snake-rotate-left`);
+        if (curr === 'down' && prev === 'right')
+          snakeElement.classList.add(`snake-rotate-right`);
+        if (curr === 'down' && prev === 'left')
+          snakeElement.classList.add(`snake-rotate-left`);
       } else {
-        if (
-          this.#snake[i - 1].direction === 'up' &&
-          this.#snake[i].direction === 'right'
-        ) {
-          snakeElement.classList.add('snake-bottom-right');
-        }
-        if (
-          this.#snake[i - 1].direction === 'up' &&
-          this.#snake[i].direction === 'left'
-        ) {
-          snakeElement.classList.add('snake-bottom-left');
-        }
-
-        if (
-          this.#snake[i - 1].direction === 'down' &&
-          this.#snake[i].direction === 'right'
-        ) {
-          snakeElement.classList.add('snake-top-right');
-        }
-        if (
-          this.#snake[i - 1].direction === 'down' &&
-          this.#snake[i].direction === 'left'
-        ) {
-          snakeElement.classList.add('snake-top-left');
-        }
         snakeElement.classList.add('snake');
+        const prev = this.#snake[i - 1].direction;
+        const curr = this.#snake[i].direction;
+        if (curr === 'left' && prev === 'down')
+          snakeElement.classList.add('snake-top-left');
+        if (curr === 'right' && prev === 'down')
+          snakeElement.classList.add('snake-top-right');
+        if (curr === 'left' && prev === 'up')
+          snakeElement.classList.add('snake-bottom-left');
+        if (curr === 'right' && prev === 'up')
+          snakeElement.classList.add('snake-bottom-right');
+        // //
+        if (curr === 'up' && prev === 'right')
+          snakeElement.classList.add('snake-top-left');
+        if (curr === 'up' && prev === 'left')
+          snakeElement.classList.add('snake-top-right');
+        if (curr === 'down' && prev === 'right')
+          snakeElement.classList.add('snake-bottom-left');
+        if (curr === 'down' && prev === 'left')
+          snakeElement.classList.add('snake-bottom-right');
       }
-
       boardElement.append(snakeElement);
     });
   }
